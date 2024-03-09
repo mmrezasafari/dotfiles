a443d957b7970a74442d8e1c1f2c1df7184435b5
mmrezasafari <mmrezadev@gmail.com>
2024-03-10 02:05:22 +0330

    add :: added custome keymap

---
+++ nvim/lua/plugins/keymaps.lua
@@ -0,0 +1,41 @@
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
+-- Increment
+keymap.set("n", "+", "<C-a>")
+keymap.set("n", "-", "<C-x>")
+
+-- Delete a word backwards
+keymap.set("n", "dw", "vb_d")
+
+-- select all
+keymap.set("n", "<C-a>", "gg<S-v>G", opts)
+
+-- jumplist
+keymap.set("n", "<C-m>", "<C-i>", opts)
+
+-- newtab
+keymap.set("n", "te", "tabedit", opts)
+keymap.set("n", "<tab>", ":tabnext<Return>", opts)
+keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
+
+-- split window
+keymap.set("n", "ss", ":split<Return>", opts)
+keymap.set("n", "ss", ":vsplit<Return>", opts)
+
+-- move window
+keymap.set("n", "sh", "<C-w>h")
+keymap.set("n", "sk", "<C-w>k")
+keymap.set("n", "sj", "<C-w>j")
+keymap.set("n", "sl", "<C-w>l")
+
+-- resize  window
+keymap.set("n", "<C-w><left>", "<C-w><")
+keymap.set("n", "<C-w><right>", "<C-w>>")
+keymap.set("n", "<C-w><up>", "<C-w>+")
+keymap.set("n", "<C-w><down>", "<C-w>-")
+
+-- Diagnostics
+keymap.set("n", "<C-j>", function()
+  vim.diagnostic.goto_next()
+end, opts)
